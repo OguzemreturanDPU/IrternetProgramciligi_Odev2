e8b94676f5e962032a2c2fadbddd07fc9ef448eb
OguzemreturanDPU <oguz.turan0@ogr.dpu.edu.tr>
2022-10-28 20:58:27 +0300

    IrternetProgramciligi_Odev2
    Sondaki Sehri Yazdırma

---
+++ 2.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+
+$sehirler= array ("balıkesir","kütahya","bursa","yalova","kocaeli");
+$sehirler=["balıkesir","kütahya","bursa","yalova","kocaeli"];
+$soneleman=count($sehirler)-1;
+echo $sehirler[$soneleman];
+
+?>
+</body>
+</html>
